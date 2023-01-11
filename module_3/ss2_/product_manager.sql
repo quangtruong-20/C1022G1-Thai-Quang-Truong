2720064e06373b5580741edfa81dd9f4295d9a0d
ghgmdr <truongtq20@gmail.com>
2023-01-11 21:08:22 +0700

    .

--- module_3/ss2_/product_manager.sql
+++ module_3/ss2_/product_manager.sql
@@ -1,11 +1,12 @@
 create database product_manager;
 
+
 CREATE TABLE customer (
     c_id INT PRIMARY KEY auto_increment,
     c_name VARCHAR(45),
     c_age INT
 );
-CREATE TABLE order1 (
+CREATE TABLE `order` (
     o_id INT PRIMARY KEY auto_increment,
     c_id INT,
     o_date DATE,
@@ -18,7 +19,7 @@ CREATE TABLE order_detail (
     p_id INT,
     PRIMARY KEY (o_id , p_id),
     FOREIGN KEY (o_id)
-        REFERENCES order1 (o_id),
+        REFERENCES `order` (o_id),
     FOREIGN KEY (p_id)
         REFERENCES product (p_id),
     od_qty INT
