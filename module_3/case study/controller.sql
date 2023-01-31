8d6a684e48f30d354857b66b9cf562caffb46af9
ghgmdr <truongtq20@gmail.com>
2023-01-31 22:50:50 +0700

    .

--- module_3/case study/controller.sql
+++ module_3/case study/controller.sql
@@ -34,14 +34,13 @@ group by  khach_hang.ma_khach_hang, hop_dong.ma_hop_dong;
 -- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
 -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt
 -- từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
-SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
-FROM dich_vu dv
-JOIN loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
-
-WHERE dv.ma_dich_vu not in ( 
+select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+from dich_vu dv
+join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+where dv.ma_dich_vu not in ( 
 select hd.ma_dich_vu  
 from hop_dong hd
-where( YEAR(hd.ngay_lam_hop_dong) = 2021 AND MONTH(hd.ngay_lam_hop_dong) BETWEEN 1 AND 3)
+where( year(hd.ngay_lam_hop_dong) = 2021 and month(hd.ngay_lam_hop_dong) between 1 and 3)
 )
 group by dv.ma_dich_vu,dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu;
 
