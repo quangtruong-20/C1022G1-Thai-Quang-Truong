196322d3033178c7f763b919cc0408a9706c0e97
ghgmdr <truongtq20@gmail.com>
2023-02-02 15:08:25 +0700

    .

--- module2/src/GiaiThua.java
+++ module2/src/GiaiThua.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class GiaiThua {
+
+    public static int giaiThua(int n){
+
+        if (n == 0) return 1;
+        else return n* giaiThua(n-1);
+
+    }
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập");
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.println(factorial(n));
+
+
+    }
 }
